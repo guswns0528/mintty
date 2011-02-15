guswns0528/mintty

commit 1ff6de5dd8f7f2236d1b548957b5aad913c49658

Fix bug in clearing of combining character cells when reusing termlines during scrolling. This was introduced in r828, due to the mistaken assumption that empty combining character cells don't need to have any structure when in fact they need to be linked correctly (via the cc_next field) in a free list. Address this by simply reducing the termline's size to the actual number of columns. Hopefully this addressesthe hang reported at http://www.cygwin.com/ml/cygwin/2010-11/msg00131.html.
git-svn-id: http://mintty.googlecode.com/svn/trunk@1078 2782a51e-c42b-11dd-927d-974589228060

termline.c
@@ -74,13 +74,10 @@ add_cc(termline *line, int col, wchar chr)
     line->size += 16 + (line->size - line->cols) / 2;
     line->chars = renewn(line->chars, line->size);
     line->cc_free = n;
-    while (n < line->size) {
-      if (n + 1 < line->size)
-        line->chars[n].cc_next = 1;
-      else
-        line->chars[n].cc_next = 0;
-      n++;
-    }
+    do
+      line->chars[n].cc_next = 1;
+    while (++n < line->size - 1);
+    line->chars[n].cc_next = 1;
   }
 
  /*
@@ -643,6 +640,22 @@ decompressline(uchar *data, int *bytes_used)
     *bytes_used = b->len;
 
   return line;
+}
+
+/*
+ * Clear a line, throwing away any combining characters.
+ */
+void
+clearline(termline *line)
+{
+  line->attr = LATTR_NORM;
+  for (int j = 0; j < line->cols; j++)
+    line->chars[j] = term.erase_char;
+  if (line->size > line->cols) {
+    line->size = line->cols;
+    line->chars = renewn(line->chars, line->size);
+    line->cc_free = 0;
+  }
 }
 
 /*

term.c
@@ -501,13 +501,8 @@ term_do_scroll(int topline, int botline, int lines, bool sb)
   termline *recycled[abs(lines)];
   void recycle(termline **src) {
     memcpy(recycled, src, sizeof recycled);
-    for (int i = 0; i < lines; i++) {
-      termline *line = recycled[i];
-      for (int j = 0; j < line->cols; j++)
-        line->chars[j] = term.erase_char;
-      line->cc_free = line->cols;
-      line->attr = LATTR_NORM;
-    }
+    for (int i = 0; i < lines; i++)
+      clearline(recycled[i]);
   }
 
   if (down) {

term.h
@@ -178,6 +178,7 @@ typedef struct {
 
 termline *newline(int cols, int bce);
 void freeline(termline *);
+void clearline(termline *);
 void resizeline(termline *, int);
 
 int sblines(void);

commit 467becc204ed0b7d5e80edec1ce8ec6775054cd6

Fix a crash due to the free list of combining character cells not being terminated correctly. This bug was introduced in r1078. Issue 250.

termline.c
@@ -77,7 +77,7 @@ add_cc(termline *line, int col, wchar chr)
     do
       line->chars[n].cc_next = 1;
     while (++n < line->size - 1);
-    line->chars[n].cc_next = 1;
+    line->chars[n].cc_next = 0;  // Terminates the free list.
   }
 
  /*